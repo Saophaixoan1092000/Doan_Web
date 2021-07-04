1a3aaf27cdc537ed46f13820ee4c137fc651b198
USER <USER@LAPTOP-IT6KI1MT>
2021-07-04 14:10:41 +0700

    Vừa thêm chút code

--- SA_SHOES/Common/CommonConStants.cs
+++ SA_SHOES/Common/CommonConStants.cs
@@ -8,5 +8,8 @@ namespace SA_SHOES.Common
     public static class CommonConStants
     {
         public static string CartSession = "CartSession";
+
+        public static string USER_Session = "CartSession";
     }
+   
 }
